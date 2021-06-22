049a1c6438a6a62a99e4b34302712ef4cbab1159
Michelle <michey.asmah21@gmail.com>
2021-06-22 17:48:38 -0400

    date format and thought model

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 1,
+      maxLength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    // use ReplySchema to validate data for a reply
+    reactions: [ReactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
