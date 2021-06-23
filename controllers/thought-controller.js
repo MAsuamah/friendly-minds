a2cbcd07df171935c068cc61c7007f704850cb6e
Michelle <michey.asmah21@gmail.com>
2021-06-23 18:56:12 -0400

    fixed remove thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -63,25 +63,14 @@ const thoughtController = {
   },
 
   removeThought({ params }, res) {
-    Thought.findOneAndDelete({ _id: params.thoughId })
-      .then(deletedThought => {
-        if (!deletedThought) {
+    Thought.findOneAndDelete({ _id: params.thoughtId })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
           return res.status(404).json({ message: 'No thought with this id!' });
         }
-        return User.findOneAndUpdate(
-          { _id: params.userId },
-          { $pull: { thoughts: params.thoughtId } },
-          { new: true }
-        );
+        res.json(dbThoughtData)
       })
-      .then(dbUserData => {
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No user found with this id!' });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch(err => res.json(err));
+      .catch(err => res.status(400).json(err));
   },
 
   addReaction({ params, body }, res) {
