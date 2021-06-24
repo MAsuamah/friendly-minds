7c427eb20e21e755ab0f16b4cc86033e780eb9a7
Michelle <michey.asmah21@gmail.com>
2021-06-23 23:27:01 -0400

    comments

--- models/Thought.js
+++ models/Thought.js
@@ -51,7 +51,7 @@ const ThoughtSchema = new Schema(
       required: true,
       trim: true
     },
-    // use ReplySchema to validate data for a reply
+    // use ReactionSchema to validate data for a reply
     reactions: [ReactionSchema]
   },
   {
