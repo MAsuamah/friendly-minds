e6f0c2c043f573a8efd359490cd33992ac93ed8c
Michelle <michey.asmah21@gmail.com>
2021-06-27 18:40:05 -0400

    user delete

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -56,17 +56,17 @@ const userController = {
 
   // delete user
   deleteUser({ params }, res) {
-  User.findOneAndDelete({ _id: params.id })
-    .then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id!' });
-        return;
-      }
-      
-      res.json(dbUserData);
-    })
-    .catch(err => res.status(400).json(err));
-  },
+    User.findOneAndDelete({ _id: params.id })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+    },
 
   addFriend({ params, body }, res) {
     User.findOneAndUpdate(
